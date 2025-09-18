aad43e6a4736ff1158f99a116e072aca5cc66170
NwaneriAndrew <nwaneriandrew@icloud.com>
2025-09-18 17:01:53 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+Bootstrap5(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
